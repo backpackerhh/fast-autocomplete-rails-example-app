f14ba072a110e362759c8d18feb66630f0da431c
David Montesdeoca <backpackerhh@gmail.com>
2014-07-18 12:55:06 +0100

    Add callbacks to add and remove data from Redis.
    * The 'data' key is an optional container for metadata you'd like
    to return when an item is matched.

--- app/models/noun.rb
+++ app/models/noun.rb
@@ -1,2 +1,26 @@
 class Noun < ActiveRecord::Base
+  after_save     :load_into_soulmate
+  before_destroy :remove_from_soulmate
+ 
+  validates_uniqueness_of :name
+ 
+  private
+
+  def loader
+    Soulmate::Loader.new('nouns')
+  end
+ 
+  def load_into_soulmate
+    loader.add(
+      'term' => name,
+      'id'   => self.id,
+      'data' => {
+        'link' => Rails.application.routes.url_helpers.noun_path(self)
+      }
+    )
+  end
+ 
+  def remove_from_soulmate
+    loader.remove('id' => self.id)
+  end
 end
